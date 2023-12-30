09a757c772fbbdedc9e1225dc045fdfc78509acd
Nisala Ravihara <116428103+NisalaRV@users.noreply.github.com>
2023-12-31 01:50:15 +0530

    added Aboutpage

--- src/view/pages/About/About.tsx
+++ src/view/pages/About/About.tsx
@@ -1,28 +1,25 @@
 import {Component} from "react";
+import c1 from '../../../assets/images/c1.png'
 
 export class About extends Component {
     render() {
         return (
-            <div className="flex">
-                <div className="h-auto pt-5 pl-10 pr-10 mx-auto">
-                    <h2 className="pb-3 text-3xl text-green-400 underline decoration-2">About Us</h2>
-                    <div className="pb-3">
-                        {/* Include your image with the appropriate path or URL */}
-                        <img
-                            className="img"
-                            src="../../../assets/logolatte.jpg"
-                            alt="About Us"
-                        />
-                    </div>
-                    <p className="pb-3 text-[9px]">
-                        At Organic Shop, we believe in the power of nature to nourish, heal, and inspire. Our journey began with a simple yet profound realization: the importance of embracing organic living for the well-being of both individuals and the planet.
-                        <br />
-                        <br />
-                        Mission:
-                        Our mission is to make organic living accessible to all, fostering a harmonious relationship between people and the environment. We are committed to offering a diverse range of high-quality, ethically sourced, and sustainable products that promote health, wellness, and a greener lifestyle.
-                    </p>
+            <section className="w-screen mt-12">
+                <div className="flex flex-1 justify-start items-center flex-col gap-6">
+                    <h5 className="font-bold mt-10 text-4xl">About</h5>
+                    <p className="text-2xl uppercase font-light">Enjoy Your Free Time</p>
                 </div>
-            </div>
+                <p className=" ml-[550px] mt-5  text-left text-1xl">Latte Korea is a Korean style cafe that serves simple, natural food – including Vegan and Gluten Free options – accompanied by authentic espresso coffee. The Latte korea café is a hybrid combining a strong coffee culture and a robust food culture under one roof in a welcoming, cozy and comfortable neighborhood café.</p>
+                <p className=" ml-[550px] mt-5 text-left text-1xl">Our Coffee – We serve a range of espresso coffees made from the finest coffee beans, roasted right here in Cape Coral, and our baristas really know their beans. With their superior technical knowledge and a passion for making amazing coffee they will make you the perfect cuppa. </p>
+                <p className=" ml-[550px] mt-5  text-left text-1xl">Our Food – Our food is all natural, simple food made with local ingredients, wherever possible, to provide you with fresher, healthier and deliciously tasty meals. We have a well stocked pastry cabinet, our food has an international flavor and we offer gluten free, vegetarian and vegan options. </p>
+                <p className=" ml-[550px] mt-5  text-left text-1xl">We also have a little attitude: Café YOU has a cool, trendy shabby chic decor and an upbeat fun atmosphere with a whole lot of colorful idioms:  </p>
+                <p className=" ml-[550px] mt-5  text-left text-1xl">Coffee & sweet 🍵Bobble tea🥤 Sushi 🍣 foods 🍜 mocktails & ice tea</p>
+                <div>
+                    <img src={c1} alt="img" className="w-[400px] -mt-[400px]  h-full ml-20 hidden rounded-r-2xl m-2.5 md:block object-cover"/>
+                </div>
+
+
+            </section>
         );
 
     }
